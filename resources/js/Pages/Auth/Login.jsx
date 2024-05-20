0b231e523f0e6cb5ca6faf61253d80a23747c27a
HarryVishmitha <vishmithathejan154@gmail.com>
2024-05-20 14:40:14 +0530

    updating

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -27,71 +27,58 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
-
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                        >
-                            Forgot your password?
+        <>
+            <Head title="Login"/>
+            <div className="d-flex justify-content-center align-items-center static-div-center">
+                <div className="border col-sm-3">
+                    <div className='branding d-flex justify-content-center align-items-center mt-2 mb-3'>
+                        <Link href={route('home')} className='nav-link d-flex align-items-center justify-content-center'>
+                            <img src="/img/wg-logo.png" alt="Logo" className="login-brand"/>
                         </Link>
-                    )}
+                    </div>
+                    <div className="login m-2">
+                        {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+                        {errors._message && <div className="text-danger">{errors._message}</div>}
+                        <form onSubmit={submit}>
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control"  id="email" placeholder="name@example.com" name='email' value={data.email} autoComplete="username" isFocused={true} onChange={(e) => setData('email', e.target.value)}/>
+                                <label htmlFor="floatingInput">Email address</label>
+                                {errors.email && <div className="text-danger">{errors.email}</div>}
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="password" className="form-control"  id="password" placeholder="password"  name="password" value={data.password} autoComplete="current-password"  onChange={(e) => setData('password', e.target.value)}/>
+                                <label htmlFor="floatingInput">Password</label>
+                                {errors.password && <div className="text-danger">{errors.password}</div>}
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" name="remember" checked={data.remember} onChange={(e) => setData('remember', e.target.checked)}/>
+                                <label className="form-check-label" htmlFor="flexCheckDefault">
+                                    Remember me
+                                </label>
+                            </div>
+                            <div className="flex items-center justify-end mt-2 mb-3">
+                                {canResetPassword && (
+                                    <Link         
+                                        href={route('password.request')}
+                                        className="nav-link"
+                                    >
+                                        Forgot your password?
+                                    </Link>
+                                )}
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
+                                <button className="btn btn-outline-primary w-100 mt-2" disabled={processing}>
+                                    Log in
+                                </button>
+                                <div className="mt-2 mb-3">
+                                    <span>
+                                        Don't have account, <Link href={route('register')} className='not-underline'>Create one</Link>
+                                    </span>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </form>
-        </GuestLayout>
+            </div>
+        </>
     );
 }
