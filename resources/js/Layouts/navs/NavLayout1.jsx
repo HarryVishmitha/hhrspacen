c3de8c7b879cc46208bf6485210d37158c88d0c7
HarryVishmitha <vishmithathejan154@gmail.com>
2024-07-21 19:03:33 +0530

    by 2119

--- resources/js/Layouts/navs/NavLayout1.jsx
+++ resources/js/Layouts/navs/NavLayout1.jsx
@@ -5,7 +5,7 @@ export default function NavLayout1() {
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid p-1">
                 <Link href={ route('home') } className="navbar-brand ms-3">
-                    <img src="img/wg-logo.png" alt="Logo" className="d-inline-block align-text-top logo"/>
+                    <img src="/img/wg-logo.png" alt="Logo" className="d-inline-block align-text-top logo"/>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbars" aria-controls="navbars" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
